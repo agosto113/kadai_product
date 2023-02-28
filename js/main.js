3885c6f1de762bbd18d0f8626cf3fb8f27645d17
hazuki <hzktmr@gmail.com>
2023-02-28 14:39:05 -0800

    エラー

---
+++ js/main.js
@@ -0,0 +1,103 @@
+jQuery(function ($) {
+
+    $('.bg-slider').bgSwitcher({
+        images: ["images/background1.jpg", "images/background2.jpg", "images/background3.jpg", "images/background4.jpg"], // 切替背景画像を指定
+        interval: 3000, // 背景画像を切り替える間隔を指定 3000=3秒
+        loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
+        shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
+        effect: "fade", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
+        duration: 500, // エフェクトの時間を指定します。
+        easing: "swing" // エフェクトのイージングをlinear,swingから指定
+    });
+
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+
+
+
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 80, "density": {
+                    "enable": true, "value_area": 800
+                }
+            }, "color": {
+                "value": "#ffffff"
+            }, "shape":
+            {
+                "type": "circle", "stroke":
+                {
+                    "width": 0, "color": "#000000"
+                }, "polygon":
+                {
+                    "nb_sides": 5
+                }, "image":
+                {
+                    "src": "img/github.svg", "width": 100, "height": 100
+                }
+            }, "opacity":
+            {
+                "value": 0.5, "random": false, "anim":
+                {
+                    "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false
+                }
+            }, "size":
+            {
+                "value": 3, "random": true, "anim":
+                {
+                    "enable": false, "speed": 40, "size_min": 0.1, "sync": false
+                }
+            }, "line_linked":
+            {
+                "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1
+            }, "move":
+            {
+                "enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract":
+                {
+                    "enable": false, "rotateX": 600, "rotateY": 1200
+                }
+            }
+        }, "interactivity":
+        {
+            "detect_on": "canvas", "events":
+            {
+                "onhover":
+                {
+                    "enable": true, "mode": "repulse"
+                }, "onclick":
+                {
+                    "enable": true, "mode": "push"
+                }, "resize": true
+            }, "modes":
+            {
+                "grab":
+                {
+                    "distance": 400, "line_linked":
+                    {
+                        "opacity": 1
+                    }
+                }, "bubble":
+                {
+                    "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3
+                }, "repulse":
+                {
+                    "distance": 200, "duration": 0.4
+                }, "push":
+                {
+                    "particles_nb": 4
+                }, "remove":
+                {
+                    "particles_nb": 2
+                }
+            }
+        }, "retina_detect": true
+    }); var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function () {
+        stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+            count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+        } requestAnimationFrame(update);
+    }; requestAnimationFrame(update);;
+
+});
